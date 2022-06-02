524c445d08ceb3199ce6d6acf9da859537d842a6
Murat Kasimov <iokasimov.m@gmail.com>
2022-06-02 21:04:08 +0700

    Add missing imporst to Kleisli module

--- Adj/Algebra/Morphism/Kleisli.hs
+++ Adj/Algebra/Morphism/Kleisli.hs
@@ -1,5 +1,8 @@
 module Adj.Algebra.Morphism.Kleisli where
 
+import Adj.Algebra.Morphism.Flat (Flat)
+import Adj.Algebra.Morphism.Dual (Dual)
+
 newtype Kleisli effect morphism source target = Kleisli (morphism source (effect target))
 
 type (-/->) = Kleisli (Flat (->))
