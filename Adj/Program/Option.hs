c9c4dc439731593e265fd58bfc83644bd4534fd0
Murat Kasimov <iokasimov.m@gmail.com>
2022-06-04 18:03:48 +0700

    Make Some and None patterns bidirectional

--- Adj/Program/Option.hs
+++ Adj/Program/Option.hs
@@ -7,6 +7,8 @@ type Option = (:+:) Terminal
 
 pattern Some :: a -> Option a
 pattern Some x <- Adoption x
+	where Some x = Adoption x
 
 pattern None :: Option a
 pattern None <- Option Terminal
+	where None = Option Terminal
