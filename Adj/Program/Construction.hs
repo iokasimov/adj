adee5fcfd5ca2a81a8ccb78d30a2c6f3e0990c67
Murat Kasimov <iokasimov.m@gmail.com>
2022-06-05 14:58:14 +0700

    Define Disbandable instances of Instruction and Construction

--- Adj/Program/Construction.hs
+++ Adj/Program/Construction.hs
@@ -1,9 +1,14 @@
 module Adj.Program.Construction where
 
 import Adj.Algebra ((.), (.:), Functor (map), (:*:) ((:*:)), type (-->), Flat (Flat))
-import Adj.Program.Disbandable ((=-))
+import Adj.Program.Disbandable (Disbandable (Primary, (=-), (-=)))
 
 newtype Construction t a = Construction (a :*: t (Construction t a))
+
+instance Disbandable (Construction t) where
+	type Primary (Construction t) a = a :*: t (Construction t a)
+	(=-) (Construction m) = m
+	(-=) m = Construction m
 
 instance Functor (-->) (-->) t => Functor (-->) (-->) (Construction t) where
 	map m = Flat .: \(Construction (x :*: xs)) ->
--- Adj/Program/Instruction.hs
+++ Adj/Program/Instruction.hs
@@ -1,9 +1,14 @@
 module Adj.Program.Instruction where
 
 import Adj.Algebra.Category ((.), (.:), Functor (map), (:+:) (Option, Adoption), type (-->), Flat (Flat))
-import Adj.Program.Disbandable ((=-))
+import Adj.Program.Disbandable (Disbandable (Primary, (=-), (-=)))
 
 newtype Instruction t a = Instruction (a :+: t (Instruction t a))
+
+instance Disbandable (Instruction t) where
+	type Primary (Instruction t) a = a :+: t (Instruction t a)
+	(=-) (Instruction m) = m
+	(-=) m = Instruction m
 
 instance Functor (-->) (-->) t => Functor (-->) (-->) (Instruction t) where
 	map (Flat m) = Flat .: \(Instruction i) -> Instruction .: case i of
