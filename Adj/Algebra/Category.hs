23533085b5dcaa3bd032c11450c72c5474a92d95
Murat Kasimov <iokasimov.m@gmail.com>
2022-06-11 14:09:26 +0700

    Refactor a bit

--- Adj/Algebra/Category.hs
+++ Adj/Algebra/Category.hs
@@ -21,20 +21,20 @@ infixl 8 -|
 > * Associativity: f . (g . h) ≡ (f . g) . h
 -}
 
-class Semigroupoid morphism where
-	(.) :: morphism between target
-		-> morphism source between
-		-> morphism source target
+class Semigroupoid m where
+	(.) :: m between target
+		-> m source between
+		-> m source target
 
 {- |
 > * Left identity: identity . f ≡ f
 > * Right identity: f . identity ≡ f
 -}
 
-class Semigroupoid morphism => Category morphism where
-	identity :: morphism source source
+class Semigroupoid m => Category m where
+	identity :: m source source
 
-	(.:) :: morphism (morphism source target) (morphism source target)
+	(.:) :: m (m source target) (m source target)
 	(.:) = identity
 
 {- |
@@ -57,36 +57,36 @@ class (Category from, Category to) => Functor from to f where
 		=> Functor (Betwixt from to) from f
 		=> Functor (Betwixt from to) from g
 		=> from source target -> from (f (g source)) (f (g target))
-	(-|-|) morphism
+	(-|-|) m
 		= map @(Betwixt from to) @from
 		. map @from @(Betwixt from to)
-		.: morphism
+		.: m
 
 	(-|-|-|)
 		:: Functor from (Betwixt from (Betwixt from to)) h
 		=> Functor (Betwixt from (Betwixt from to)) (Betwixt (Betwixt from to) to) g
 		=> Functor (Betwixt (Betwixt from to) to) to f
 		=> from source target -> to (f (g (h source))) (f (g (h target)))
-	(-|-|-|) morphism
+	(-|-|-|) m
 		= map @(Betwixt (Betwixt from to) to) @to
 		. map @(Betwixt from (Betwixt from to)) @(Betwixt (Betwixt from to) to)
 		. map @from @(Betwixt from (Betwixt from to))
-		.: morphism
+		.: m
 
-class Component morphism f g where
-	component :: morphism (f object) (g object)
+class Component m f g where
+	component :: m (f object) (g object)
 
 {- |
-> * Associativity: (-|) morphism . component = component . (-|) morphism
+> * Associativity: (-|) m . component = component . (-|) m
 -}
 
 class (Functor from to f, Functor from to g) => Transformation from to f g where
 	transformation :: from source target -> to (f source) (g target)
 
-newtype Flat morphism source target = Flat (morphism source target)
+newtype Flat m source target = Flat (m source target)
 
-instance Casting (Flat morphism source) where
-	type Primary (Flat morphism source) target = morphism source target
+instance Casting (Flat m source) where
+	type Primary (Flat m source) target = m source target
 	(=-) (Flat m) = m
 	(-=) m = Flat m
 
@@ -96,10 +96,10 @@ instance Semigroupoid morhism => Semigroupoid (Flat morhism) where
 instance Category morhism => Category (Flat morhism) where
 	identity = Flat identity
 
-newtype Dual morphism source target = Dual (morphism target source)
+newtype Dual m source target = Dual (m target source)
 
-instance Casting (Dual morphism target) where
-	type Primary (Dual morphism target) source = morphism source target
+instance Casting (Dual m target) where
+	type Primary (Dual m target) source = m source target
 	(=-) (Dual m) = m
 	(-=) m = Dual m
 
@@ -109,27 +109,30 @@ instance Semigroupoid morhism => Semigroupoid (Dual morhism) where
 instance Category morhism => Category (Dual morhism) where
 	identity = Dual identity
 
-newtype Kleisli effect morphism source target = Kleisli (morphism source (effect target))
+newtype Kleisli f m source target =
+	Kleisli (m source (f target))
 
-instance Casting (Kleisli effect morphism source) where
-	type Primary (Kleisli effect morphism source) target = morphism source (effect target)
+instance Casting (Kleisli f m source) where
+	type Primary (Kleisli f m source) target =
+		m source (f target)
 	(=-) (Kleisli m) = m
 	(-=) m = Kleisli m
 
-instance Functor (Kleisli functor target) target functor
-	=> Semigroupoid (Kleisli functor target) where
+instance Functor (Kleisli f target) target f
+	=> Semigroupoid (Kleisli f target) where
 		g . Kleisli f = Kleisli .: map g . f
 
-type family Covariant x source target functor where
-	Covariant Functor source target functor =
-		Functor (Flat source) (Flat target) functor
+type family Covariant x source target f where
+	Covariant Functor source target f =
+		Functor (Flat source) (Flat target) f
 
-type family Contravariant x source target functor where
-	Contravariant Functor source target functor =
-		Functor (Flat source) (Dual target) functor
+type family Contravariant x source target f where
+	Contravariant Functor source target f =
+		Functor (Flat source) (Dual target) f
 
-type family Semimonoidal x from to m f where
-	Semimonoidal Functor from to m f = Component (Flat m) (Day (Flat m) f f from to) f
+type family Semimonoidal x from to morhism f where
+	Semimonoidal Functor from to morhism f =
+		Component (Flat morhism) (Day (Flat morhism) f f from to) f
 
 -- TODO: we need a monoidal functor here
 -- instance Category (Kleisli functor target) where
@@ -174,14 +177,14 @@ instance Functor (-->) (-->) Identity where
 	map (Flat m) = Flat .: \case
 		Identity x -> Identity .: m x
 
-data Day morphism f g from to result where
+data Day m f g from to result where
 	Day :: from (f left) (g right)
-		-> morphism (to left right) result
-		-> Day morphism f g from to result
+		-> m (to left right) result
+		-> Day m f g from to result
 
 instance Functor (-->) (-->) (Day (-->) f g from to) where
-	map morphism = Flat .: \case
-		Day from to -> Day from .: morphism . to
+	map m = Flat .: \case
+		Day from to -> Day from .: m . to
 
 instance Functor (-->) (-->) ((:*:.) left) where
 	map (Flat m) = Flat .: \case
@@ -203,13 +206,13 @@ instance Functor (-->) (-->) ((.:+:) right) where
 
 instance Component (-->) (Day (-->) ((:+:.) left) ((:+:.) left) (:*:) (:*:)) ((:+:.) left) where
 	component = Flat .: \case
-		Day (Flat (That left) :*: Flat (That right)) (Flat morphism) -> Flat . That .: morphism (left :*: right)
+		Day (Flat (That left) :*: Flat (That right)) (Flat m) -> Flat . That .: m (left :*: right)
 		Day (Flat (This left) :*: _) _ -> Flat . This .: left
 		Day (_ :*: Flat (This right)) _ -> Flat . This .: right
 
 instance Component (-->) (Day (-->) ((.:+:) right) ((.:+:) right) (:*:) (:*:)) ((.:+:) right) where
 	component = Flat .: \case
-		Day (Dual (This left) :*: Dual (This right)) (Flat morphism) -> Dual . This .: morphism (left :*: right)
+		Day (Dual (This left) :*: Dual (This right)) (Flat m) -> Dual . This .: m (left :*: right)
 		Day (Dual (That left) :*: _) _ -> Dual . That .: left
 		Day (_ :*: Dual (That right)) _ -> Dual . That .: right
 
