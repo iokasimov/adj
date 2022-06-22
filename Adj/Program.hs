cbcde34a3ec322ebff63b849fb35976098c05218
Murat Kasimov <iokasimov.m@gmail.com>
2022-06-22 23:20:52 +0700

    Include Controlflow and Datastructure modules to Program

--- Adj/Program.hs
+++ Adj/Program.hs
@@ -3,3 +3,5 @@ module Adj.Program (module Exports) where
 import Adj.Program.Generation as Exports
 import Adj.Program.Maybe as Exports
 import Adj.Program.Progress as Exports
+import Adj.Program.Datastructure as Exports
+import Adj.Program.Controlflow as Exports
