aa8434151de29a45f23e5c525b633dd6974033c4
Murat Kasimov <iokasimov.m@gmail.com>
2022-07-31 14:08:17 +0700

    Leave a note about Functor instance on Lens category

--- Adj/Program/Controlflow/Implementation/Optics.hs
+++ Adj/Program/Controlflow/Implementation/Optics.hs
@@ -1,8 +1,11 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
 module Adj.Program.Controlflow.Implementation.Optics where
 
 import Adj.Auxiliary ((=-), type (=!?=), FG_ (FG_), FFGHH__ (FFGHH__), FFGHHI__ (FFGHHI__))
-import Adj.Algebra.Category (type (<--), type (-->), type (:+:>), (.), (.:), Opposite (Opposite), Straight (Straight), Identity (Identity), extract)
+import Adj.Algebra.Category (Semigroupoid ((.)), Category (identity), type (<--), type (-->), type (:+:>), (.), (.:), Opposite (Opposite), Straight (Straight), Identity (Identity), (->>-), extract)
 import Adj.Algebra.Set ((:+:) (This, That), (:*:) ((:*:)))
+import Adj.Program.Primitive.Maybe (Maybe, pattern Some, pattern None)
 import Adj.Program.Controlflow.Implementation.Store (Store)
 
 type Lens queried required = FFGHH__ (-->)
@@ -19,6 +22,26 @@ change (FFGHH__ lens) f source =
 	let (Opposite (FFGHHI__ store)) = lens =- Identity source in
 	let (q :*: r) = (=-) . (=-) .: store in
 	r =- f q
+
+-- TODO: simplify, it looks awful
+instance Semigroupoid (Lens Maybe Identity) where
+	FFGHH__ to . FFGHH__ from = FFGHH__ . Straight .: \case
+		Identity source -> case from =- Identity source of
+			Opposite (FFGHHI__ (FG_ (Straight (Some between :*: Straight mb)))) ->
+				let (Opposite (FFGHHI__ (FG_ (Straight (target :*: Straight mt))))) = to =- Identity between in
+				Opposite (FFGHHI__ (FG_ (Straight (target :*: Straight (mb . Identity . mt)))))
+			Opposite (FFGHHI__ (FG_ (Straight (_ :*: _)))) ->
+				Opposite (FFGHHI__ (FG_ (Straight (None :*: (Straight .: \_ -> source)))))
+
+instance Category (Lens Maybe Identity) where
+	identity = FFGHH__ . Straight .: \(Identity source) -> Opposite
+		-- TODO: replace =- with extract
+		(FFGHHI__ (FG_ (Straight (Some source :*: Straight .: (=-) . identity))))
+
+-- TODO: to proceed with this instance we should explore Store capabilities more
+-- instance Functor (-->) (-->) f => Functor (Lens Maybe Identity) (Lens Maybe Identity) f where
+	-- map (FFGHH__ lens) = FFGHH__ . Straight .: \(Identity fs) ->
+		-- let f_lens :: f (Store Maybe Identity target source) = (=-) lens . Identity ->>- fs in
 
 type Prism available set subset =
 	set --> (((:+:>) (available subset) =!?= (<--) (available subset)) set)
